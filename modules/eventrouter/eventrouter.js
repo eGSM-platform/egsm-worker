74e9c2f491ab137472c374ffd3584980f3a32f1a
szabolcsgarda <gardas86@gmail.com>
2022-09-01 21:29:11 +0200

    Eventrouter bindings added

--- modules/eventrouter/eventrouter.js
+++ modules/eventrouter/eventrouter.js
@@ -2,13 +2,14 @@ var xml2js = require('xml2js');
 
 var mqtt = require("./mqttconnector")
 var LOG = require('../auxiliary/LogManager')
-var egsm = require('../egsmengine/egsmengine')
+var egsm = require('../egsmengine/egsmengine');
+const { unsubscribeTopic, subscribeTopic } = require('./mqttconnector');
 
 module.id = "EVENTR"
 
 let ENGINES = new Map(); //ENGINE_ID -> [DEFAULT_BROKER, onMessageReceived]
 let SUBSCRIPTIONS = new Map(); //{HOST, PORT, TOPIC}-> [ENGINE_ID]
-let ARTIFACTS = new Map() //ENGINE_ID -> [{ARTIFACT_NAME, BROKER, HOST, BINDING, UNBINDING}]
+let ARTIFACTS = new Map() //ENGINE_ID -> [{ARTIFACT_NAME, BROKER, HOST, BINDING, UNBINDING, ID}]
 let STAKEHOLDERS = new Map() //Engine_ID -> [STAKEHOLDER_NAME, PROCESS_ID, BROKER, HOST]
 
 function createSubscription(engineid, topic, hostname, port) {
@@ -20,7 +21,9 @@ function createSubscription(engineid, topic, hostname, port) {
     }
 
     //Check if the engine is already subscribed to that topic
-    if (SUBSCRIPTIONS.get(key).find(engineid)) {
+//var asd = []
+//asd.indexOf
+    if (SUBSCRIPTIONS.has(key) && SUBSCRIPTIONS.get(key).indexOf(engineid) != -1) {
         LOG.logWorker('WARNING', `Engine [${engineid}] is already subscribed to ${hostname}:${port} -> ${topic}`, module.id)
         return
     }
@@ -58,15 +61,16 @@ function deleteSubscription(engineid, topic, hostname, port) {
 module.exports = {
     setEngineDefaults(engineid, hostname, port, onMessageReceived) {
         LOG.logWorker('DEBUG', `setDefaultBroker called: ${engineid} -> ${hostname}:${port}`, module.id)
-        ENGINES.set(engineid, { hostname, port, onMessageReceived })
+        ENGINES.set(engineid, { hostname: hostname, port: port, onMessageReceived: onMessageReceived })
     },
 
-
-
-    initConnections: function (engineid, bindingfile, hostname, port) {
+    initConnections: function (engineid, bindingfile) {
         var parseString = xml2js.parseString;
         parseString(bindingfile, function (err, result) {
 
+            if (err) {
+                console.log('Error while parsing biding file...')
+            }
             //Read Artifacts
             //Check if the engine id is defined in the map as a key
             if (!ARTIFACTS.has(engineid)) {
@@ -74,7 +78,7 @@ module.exports = {
             }
 
             //Iterate through artifacts and add them to the map
-            var ra = bindingfile['martifact:definitions']['martifact:mapping'][0]['martifact:artifact'];
+            var ra = result['martifact:definitions']['martifact:remoteArtifact'];
             for (var artifact in ra) {
                 var br = [];
                 for (var aid in ra[artifact]['martifact:bindingEvent']) {
@@ -87,7 +91,8 @@ module.exports = {
                 ARTIFACTS.get(engineid).push({
                     name: ra[artifact]['$'].name, bindingEvents: br,
                     unbindingEvents: ur, broker: ra[artifact]['$'].broker_host || undefined,
-                    port: ra[artifact]['$'].port || undefined
+                    port: ra[artifact]['$'].port || undefined,
+                    id: ''
                 })
             }
 
@@ -98,9 +103,14 @@ module.exports = {
             }
 
             //Iterate through stakeholders and add them to the map
-            var stakeHolders = bindingfile['martifact:definitions']['martifact:stakeholder'];
+            var stakeHolders = result['martifact:definitions']['martifact:stakeholder'];
             for (var key in stakeHolders) {
-                STAKEHOLDERS.get(engineid).push(stakeHolders)
+                STAKEHOLDERS.get(engineid).push({
+                    name: stakeHolders[key]['$'].name,
+                    instance: stakeHolders[key]['$'].processInstance,
+                    host: stakeHolders[key]['$'].broker_host || ENGINES.get(engineid).hostname,
+                    port: stakeHolders[key]['$'].port || ENGINES.get(engineid).port
+                })
                 createSubscription(engineid,
                     stakeHolders[key]['$'].name + '/' + stakeHolders[key]['$'].processInstance,
                     stakeHolders[key]['$'].broker_host || ENGINES.get(engineid).hostname,
@@ -116,11 +126,55 @@ module.exports = {
             return;
         }
 
-        //Check if the message is responsible for 
-        //Forward message to the appropriate engine(s)
-        SUBSCRIPTIONS.get(key).forEach(item => {
-            ENGINES.get(item).onMessageReceived(hostname, port, topic, message)
-        })
+        var elements = topic.split('/')
+        var subscribers = SUBSCRIPTIONS.get(key)
+        var msgJson = JSON.parse(message.toString())
+
+        for (var engine in subscribers) {
+            var stakeholders = STAKEHOLDERS.get(engine)
+            var artifacts = ARTIFACTS.get(engine)
+            //Check if the event is from Stakeholder -> do binding/unbinding
+            if (elements.length == 2) {
+                //Iterate through the engine's skateholders and look for match
+                //If a stakeholder found and it is verified that the message is coming from a stakeholder
+                //then iterating through the artifacts and their binding and unbinding events
+                //Performing subscribe and/or unsubscribe operations if any event match found
+                for (var stakeholder in stakeholders) {
+                    if (elements[0] == stakeholder.name && elements[1] == stakeholder.instance && hostname == stakeholder.host, port == stakeholder.port) {
+                        for (var artifact in artifacts) {
+                            //Binding events
+                            for (var bindigEvent in artifact.bindingEvents) {
+                                if (msgJson.event.payloadData.eventid == bindigEvent) {
+                                    //Unsubscribing from old artifact topic if it exists
+                                    if (artifact.id != '') {
+                                        deleteSubscription(engine, artifact.name + '/' + artifact.id + '/status', hostname, port)
+                                    }
+                                    artifact.id = msgJson.event.payloadData.data
+                                    if (artifact.id != '') {
+                                        subscribeTopic(engine, port, artifact.name + '/' + artifact.id + '/status')
+                                    }
+                                }
+                            }
+                            //Unbinding events
+                            for (var unbindigEvent in artifact.unbindingEvents) {
+                                if (msgJson.event.payloadData.eventid == unbindigEvent) {
+                                    if (artifact.id != '') {
+                                        deleteSubscription(engine, artifact.name + '/' + artifact.id + '/status', hostname, port,)
+                                        artifact.id = ''
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            //Check if the event is from Artifact and forward it to the engine
+            else if (elements.length == 3 && elements[2] == 'status') {
+                //Forward message to the engine
+                ENGINES.get(engine).onMessageReceived(hostname, port, topic,
+                    { parameters: { name: (JSON.parse(message.toString())).event.payloadData.eventid, value: '' } })
+            }
+        }
     },
 
     processPublish: function (engineid, data) {
